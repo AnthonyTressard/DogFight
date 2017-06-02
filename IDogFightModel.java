74d262c8f840b406771cebfda8c44a0a95eee076
PhilippeBurlet <philippe.burlet@viacesi.Fr>
2017-06-02 15:50:37 +0200

    Avec Classe

--- IDogFightModel.java
+++ IDogFightModel.java
@@ -1,32 +1,18 @@
 
 public interface IDogFightModel {
 	
-}
-	
-public IArea getArea() {
-	
-}
-	
-public void buildArea(Dimension:dimension) {
-	
-}
+	public IArea getArea();
 
-public void addMobile(IMobile:Mobile) {
-	
-}
-	
-public void removeMobile(Mobile:IMobile) {
-	
-}
-	
-public ArrayList<IMobile> getMobiles() {
-	
-}
-	
-public IMobile getMobileByPlayer(int player) {
-	
-}
-	
-public void setMobileHavesMoved(){
+	public void buildArea(Dimension:dimension);
+
+	public void addMobile(IMobile:Mobile);
+
+	public void removeMobile(Mobile:IMobile);
+
+	public ArrayList<IMobile> getMobiles();
+
+	public IMobile getMobileByPlayer(int player);
+
+	public void setMobileHavesMoved();
 	
 }

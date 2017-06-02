672bde727d3d3c6a1818d4b7b5f16e2a04177ed6
PhilippeBurlet <philippe.burlet@viacesi.Fr>
2017-06-02 15:42:57 +0200

    Avant classe

--- IDogFightModel.java
+++ IDogFightModel.java
@@ -1,17 +1,32 @@
 
 public interface IDogFightModel {
-	public IArea getArea();
 	
-	public void buildArea(Dimension:dimension);
+}
+	
+public IArea getArea() {
+	
+}
+	
+public void buildArea(Dimension:dimension) {
+	
+}
 
-	public void addMobile(IMobile:Mobile);
+public void addMobile(IMobile:Mobile) {
 	
-	public void removeMobile(Mobile:IMobile);
+}
 	
-	public ArrayList<IMobile> getMobiles();
+public void removeMobile(Mobile:IMobile) {
 	
-	public IMobile getMobileByPlayer(int player);
+}
 	
-	public void setMobileHavesMoved();
+public ArrayList<IMobile> getMobiles() {
+	
+}
+	
+public IMobile getMobileByPlayer(int player) {
+	
+}
+	
+public void setMobileHavesMoved(){
 	
 }
